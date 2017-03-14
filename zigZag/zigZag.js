c28871f4ab9f53276e4f447c05afd93a75de822e
Jackie <jackieh.bee@gmail.com>
2017-03-14 13:52:17 -0700

    implement zigzag function

---
+++ zigZag/zigZag.js
@@ -0,0 +1,47 @@
+ 
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string text, int nRows);
+// convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+// Credit: LeetCode
+
+
+var convert = function(s, numRows) {
+  var rows = {};
+  var count = 0;
+  var increase = true;
+  
+  for (var i = 0; i < s.length; i++) {
+    if (!rows[count]) {
+      rows[count] = [];
+    }
+    rows[count].push(s[i]);
+    
+    if ((count + 1) === numRows) {
+      increase = false;
+    }
+    
+    if ((count - 1) < 0) {
+      increase = true;
+    }
+    
+    if (increase) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  
+  var returnString = [];
+  for (var key in rows) {
+    returnString.push(rows[key].join(''));
+  }
+  return returnString.join('');
+};
