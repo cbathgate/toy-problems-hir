efc0a1ddce1b871e0220a2195714e72c9eecf7a8
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-03-28 13:43:01 -0700

    (restoreIP) create new toy problem folder

---
+++ restoreIP/restoreIP.js
@@ -0,0 +1,12 @@
+// Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+// For example:
+// Given "25525511135",
+
+// return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+
+// A valid IP address must be in the form of xxx.xxx.xxx.xxx, where xxx is a number from 0-255. 
+
+var restoreIpAddresses = function(string) {
+
+};
