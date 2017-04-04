e01c47fcb3b0baf8465734d27342adc8631dffec
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-04-04 10:21:06 -0700

    (productAllNums) implement with O(n) time and space

--- productAllNums/productAllNums.js
+++ productAllNums/productAllNums.js
@@ -4,6 +4,23 @@
 // Constraints: O(n) time and space, do not use division
 // Edge cases: 0 in the array
 
-var productAllNums = function() {
-  // TODO: YOUR CODE HERE
+var productAllNums = function(arr) {
+
+  var result = [];
+  var productSoFar = 1;
+
+  for(var i = 0; i < arr.length; i++) {
+    result.push(productSoFar);
+    productSoFar *= arr[i]
+  }
+
+  productSoFar = 1;
+
+  for(var i = arr.length - 1; i >= 0; i--) {
+    result[i] = result[i] * productSoFar;
+    productSoFar *= arr[i];
+  }
+
+  return result;
+
 }
