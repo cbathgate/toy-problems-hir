ba1a283b1c07e0fb9784f5a0620f220d63933405
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-04-04 09:37:52 -0700

    (productAllNums) initialized folder

---
+++ productAllNums/productAllNums.js
@@ -0,0 +1,9 @@
+// Write a function productAllNums() that takes an array of integers and 
+// returns an array of the products.
+
+// Constraints: O(n) time and space, do not use division
+// Edge cases: 0 in the array
+
+var productAllNums = function() {
+  // TODO: YOUR CODE HERE
+}
