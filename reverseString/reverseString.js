7141c0e1a762d9cddbe431459ae63b2892f0c913
Cynthia Bathgate <c.bathgate1@gmail.com>
2017-05-15 21:27:17 -0700

    (reverseString) refactor to accomodate reverseWord

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,10 +1,13 @@
-var reverseString = function(str) {
-  var stringArray = str.split('');
+var reverseString = function(str, sep) {
+  sep = sep || '';
+  var stringArray = str.split(sep);
   for (var i = 0; i < ((stringArray.length - 1) / 2); i++) {
     var temp = stringArray[i];
     stringArray[i] = stringArray[(stringArray.length - 1) - i];
     stringArray[(stringArray.length - 1) - i] = temp;
   }
 
-  return stringArray.join('');
+  return stringArray.join(sep);
 }
+
+reverseString('california is the best', ' ');
